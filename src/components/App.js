7beb382237ca5c2355741cf9ed743dac4e6b67be
Gleidy Mendoza <gleidy.mendoza@ntrust.com>
2017-11-24 18:51:54 -0800

    3.3 Connect - mapStateToProps()

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+//Redux Store
 class App extends Component {  
   render() {
+  	console.log('Props', this.props)
     return (
       <div>
         Hola mundo
@@ -9,4 +11,28 @@ class App extends Component {
     )
   }
 }
-export default App
+
+/*
+Map our Redux state to our Component props
+where calendar (state)
+return will be passed to our component as long as we pass 
+mapStateToProps as 1st argument to connect.
+*/
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+export default connect(mapStateToProps)(App)
